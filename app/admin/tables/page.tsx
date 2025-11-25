d881965a2f191fa7fc442d8ba6e2463752dd5301
Sylvère O. AKAMBI <akambi.sylv@gmail.com>
2025-11-25 15:12:08 +0100

    fix error

--- app/admin/tables/page.tsx
+++ app/admin/tables/page.tsx
@@ -1,6 +1,7 @@
 import { getTables } from '@/lib/blob';
 import TablesClientPage from './tables-client';
 
+//dd
 export default async function TablesPage() {
   const tables = await getTables();
   
